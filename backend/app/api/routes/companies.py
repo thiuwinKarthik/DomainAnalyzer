7dffdd592154c267701808424d088e5d54164d0c
thiuwinkarthik <thiruwinkarthik4@gmail.com>
2025-12-18 15:25:03 +0530

    Initial commit - full project

--- backend/app/api/routes/companies.py
+++
@@ -1,8 +0,0 @@
-# from fastapi import APIRouter
-# from app.services.company_service import CompanyService
-
-# router = APIRouter()
-
-# @router.get("/companies")
-# def get_companies():
-#     return CompanyService.get_all_companies()
